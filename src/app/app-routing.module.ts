c1fc61c401177bb5707f9f32e9d7980fefd3bb39
GomerClaure <gomerclaureb@gmail.com>
2023-12-16 13:07:09 -0400

    Arreglando ruta home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import {AnfitrionModule} from "./modulos/anfitrion/anfitrion.module";
 import {AuthActiveGuard} from "./guard/auth-active.guard";
 import {AdminGuard} from "./guard/admin.guard";
-//import {HuespedModule} from "./modulos/huesped/huesped.module";
 import {EstructuraComponent} from "./componentes/estructura/estructura.component";
 const routes: Routes = [
+  
+  { 
+    path: '', 
+    redirectTo: 'home-page', 
+    pathMatch: 'full'
+  },
   {
     path: 'home-page',
     loadChildren: () => import('./modulos/home/home.module').then(m => m.HomeModule),
   },
-  
   {
     path: 'anfitrion',
     canActivate: [AuthActiveGuard,AdminGuard],
