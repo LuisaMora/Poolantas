d6ee150da17dead1df3745d0482ba6c3cbc7b997
Luisa Mora <luisamora895@gmail.com>
2023-12-16 22:03:36 -0400

    add mostrar categorias de plantas

--- src/app/modulos/anfitrion/ver-plantas/ver-planta.component.ts
+++ src/app/modulos/anfitrion/ver-plantas/ver-planta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Plantas } from '../../../../../src/app/modelos/plantas';
+import {AnfitrionService} from "../../../service/anfitrion.service";
 
 @Component({
   selector: 'app-ver-planta',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-planta.component.scss']
 })
 export class VerPlantaComponent implements OnInit {
+  constructor(private anfitrionService: AnfitrionService,) { }
+  categoria: Plantas[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    this.getPropiedades();
   }
-
+  getPropiedades() {
+    this.anfitrionService.getCategorias().subscribe((data) => {
+      this.categoria = data;
+    });
+  }
 }
--- src/app/modulos/home/lista-categorias/lista-categorias.component.ts
+++ src/app/modulos/home/lista-categorias/lista-categorias.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { INPUT_APARIENCIA } from 'src/app/config/constantes';
 import { Plantas } from '../../../../../src/app/modelos/plantas';
 import {AnfitrionService} from "../../../service/anfitrion.service";
+import { CategoryService } from 'src/app/service/category.service';
+import { Category } from 'src/app/modelos/category';
+import { Subject, takeUntil } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-lista-categorias',
@@ -10,14 +14,16 @@ import {AnfitrionService} from "../../../service/anfitrion.service";
 })
 export class ListaCategoriasComponent implements OnInit {
   apariencia = INPUT_APARIENCIA;
-  constructor(private anfitrionService: AnfitrionService,) { }
-  categoria: Plantas[] = [];
+  constructor(private anfitrionService: AnfitrionService,private categoryService: CategoryService ) { }
+  categoria: Category[] = [];
+  categoyInstance = new Subject();
+  base_backend_url: string = environment.backendStorageUrl;
 
   ngOnInit(): void { 
-    this.getPropiedades();
+    this.getCategorias();
   }
-  getPropiedades() {
-    this.anfitrionService.getCategorias().subscribe((data) => {
+  getCategorias() {
+    this.categoryService.getAll().pipe(takeUntil(this.categoyInstance)).subscribe((data:any) => {
       this.categoria = data;
     });
   }
