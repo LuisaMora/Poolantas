8bc06d055c43ec7de3889504cabb15e6382df4c0
GomerClaure <gomerclaureb@gmail.com>
2023-12-16 23:57:30 -0400

    Registrando videos

--- src/app/modulos/anfitrion/anfitrion-videos/registrar-video/registrar-video.component.ts
+++ src/app/modulos/anfitrion/anfitrion-videos/registrar-video/registrar-video.component.ts
@@ -4,6 +4,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { VideoService } from 'src/app/service/video.service';
 import { Location } from '@angular/common';
 
+
+// ... importaciones
+
 @Component({
   selector: 'app-registrar-video',
   templateUrl: './registrar-video.component.html',
@@ -11,38 +14,46 @@ import { Location } from '@angular/common';
 })
 export class RegistrarVideoComponent implements OnInit {
   formRegistro: FormGroup;
-  
-  constructor(private videoService: VideoService,
+
+  constructor(
+    private videoService: VideoService,
     private fb: FormBuilder,
     private snackBar: MatSnackBar,
-    private location: Location) {
+    private location: Location
+  ) {
     this.formRegistro = this.fb.group({
-      iframe_code: ['', []],
+      iframeCode: ['', [Validators.required]],
     });
-
-   }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {}
+
   guardarDatos() {
-    console.log("Entra")
+    console.log(this.formRegistro.value);
     if (this.formRegistro.valid) {
-      const datosFormulario = new FormData();
-      datosFormulario.append('iframe_code', this.formRegistro.get('iframe_code')?.value);
-      console.log(this.formRegistro.value);
-      this.videoService.store(datosFormulario).subscribe((data: any) => {
-        if (data) {
-          this.showSnackBar('Video registrado correctamente', 'success');
-          // this.location.back();
-        }else{
-          this.showSnackBar('Error al registrar el video', 'error');
-        }
-      }, (error: any) => {
-        console.log(error);
-        this.showSnackBar('Error al registrar el video', 'error');
+      const iframeCode = this.formRegistro.get('iframeCode')?.value?.trim();
+
+      if (iframeCode) {
+        const datosFormulario = new FormData();
+        datosFormulario.append('iframe_code', iframeCode);
+
+        this.videoService.store(datosFormulario).subscribe(
+          (data: any) => {
+            if (data) {
+              this.showSnackBar('Video registrado correctamente', 'success');
+              this.location.back();
+            } else {
+              this.showSnackBar('Error al registrar el video', 'error');
+            }
+          },
+          (error: any) => {
+            console.log(error);
+            this.showSnackBar('Error al registrar el video', 'error');
+          }
+        );
+      } else {
+        this.showSnackBar('El código del iframe no puede estar vacío', 'error');
       }
-      );
     }
   }
 
@@ -56,3 +67,4 @@ export class RegistrarVideoComponent implements OnInit {
   }
 
 }
+
