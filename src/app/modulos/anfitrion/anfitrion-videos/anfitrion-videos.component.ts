d51e121e7c04fe6b0402838aa1e856715cec3a92
Luisa Mora <luisamora895@gmail.com>
2023-12-16 23:13:51 -0400

    add videos

--- src/app/modulos/anfitrion/anfitrion-videos/anfitrion-videos.component.ts
+++ src/app/modulos/anfitrion/anfitrion-videos/anfitrion-videos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-anfitrion-videos',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anfitrion-videos.component.scss']
 })
 export class AnfitrionVideosComponent implements OnInit {
+  listaVideos: { titulo: string; enlace: string }[] = [
+    { titulo: 'Video 1', enlace: 'https://www.youtube.com/embed/MjhfRArh1-0?si=xE0KdCAPWi05UU23' },
+    { titulo: 'Video 2', enlace: 'https://www.youtube.com/embed/OH4z9lR9WCg?si=xE0KdCAPWi05UU23' },
+    // Agrega más videos según sea necesario
+  ];
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    // Sanitizar las URLs de los videos
+    this.listaVideos.forEach(video => {
+      video.enlace = this.sanitizer.bypassSecurityTrustResourceUrl(video.enlace) as string;
+    });
   }
-
 }
