021c3966f553728c2a66ba0483f4b7bed29fe999
Luisa Mora <luisamora895@gmail.com>
2023-12-16 22:47:33 -0400

    add mostrar plantas en la pantalla principal

--- src/app/modulos/home/planta/planta.component.ts
+++ src/app/modulos/home/planta/planta.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Plantas } from '../../../../../src/app/modelos/plantas';
 import {AnfitrionService} from "../../../service/anfitrion.service";
+import { environment } from 'src/environments/environment';
+import { PlantService } from 'src/app/service/plant.service';
+import { Subject, takeUntil } from 'rxjs';
+
 @Component({
   selector: 'app-planta',
   templateUrl: './planta.component.html',
   styleUrls: ['./planta.component.scss']
 })
 export class PlantaComponent implements OnInit {
-  constructor(private anfitrionService: AnfitrionService,) { }
-  categoria: Plantas[] = [];
+  constructor(private anfitrionService: AnfitrionService,private plantasService: PlantService) { }
+  plantas: Plantas[] = [];
+  categoyInstance = new Subject();
+  base_backend_url: string = environment.backendStorageUrl;
 
   ngOnInit(): void { 
-    this.getPropiedades();
+    this.cargarPlantas();
   }
-  getPropiedades() {
-    this.anfitrionService.getCategorias().subscribe((data) => {
-      this.categoria = data;
+  cargarPlantas() {
+    this.plantasService.get().pipe(takeUntil(this.categoyInstance)).subscribe((data:any) => {
+      console.log(data);
+      this.plantas = data;
     });
   }
 
